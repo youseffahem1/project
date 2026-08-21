bc991c39fb9131bcca0f4fc84565253f64fba6a1
youseffahem1 <youseffahem80@gmail.com>
2026-08-21 23:24:35 +0300

    new15

--- app/main.py
+++ app/main.py
@@ -42,7 +42,7 @@ app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "https://luckyspiin.onrender.com",
+        "https://project-wae1.onrender.com",
     ],
     allow_credentials=True,
     allow_methods=["*"],
